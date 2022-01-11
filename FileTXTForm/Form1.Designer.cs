f3b513cfec596804f07e121155e1371b4d636801
doroshinsky <doroshinsky@mail.ru>
2022-01-11 11:15:33 +0300

    Пример

--- FileTXTForm/Form1.Designer.cs
+++ FileTXTForm/Form1.Designer.cs
@@ -50,6 +50,7 @@ namespace FileTXTForm
             this.button1.TabIndex = 1;
             this.button1.Text = "Ввод в файл";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,6 +60,7 @@ namespace FileTXTForm
             this.button2.TabIndex = 2;
             this.button2.Text = "Вывод из файла";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
--- FileTXTForm/Form1.cs
+++ FileTXTForm/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FileTXTForm
 {
@@ -15,6 +11,49 @@ namespace FileTXTForm
         public Form1()
         {
             InitializeComponent();
+        }
+        
+            public void FileOutput()
+            {
+                FileInfo fileF = new FileInfo(@"f.txt");
+                string path = @"D:\KPYP_LECTION\KPYP_LECTION\FileTXTForm\f.txt";
+                FileInfo fileinfo = new FileInfo(path);
+                if (fileinfo.Exists)
+                {
+                    StreamReader sr = new StreamReader(@"f.txt");
+                    string line;
+                    using (StreamWriter sw = new StreamWriter("g.txt"))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            sw.WriteLine(line);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Данного файла не существует", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            public string FileReturn()
+            {
+                using (FileStream fstream = File.OpenRead(@"f.txt"))
+                {
+                    byte[] array = new byte[fstream.Length];
+                    fstream.Read(array, 0, array.Length);
+                    string textFromFile = System.Text.Encoding.UTF8.GetString(array);
+                    return textFromFile;
+                }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileOutput();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(FileReturn());
         }
     }
 }
