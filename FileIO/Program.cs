5bc66694c9382d514757e0b825e3bfcb92aab5d2
doroshinsky <doroshinsky@mail.ru>
2022-01-11 10:20:27 +0300

    Пример по файловому выводу

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
-namespace FileIO
+namespace ConsoleApplication1
+
 {
-    class Program
+    class Files
     {
-        static void Main(string[] args)
+        static void Main()
+
         {
+            var adress = File.Create("D:\\KPYP_LECTION\\KPYP_LECTION\\FileIO\\Primer.txt");//создание файла
+            adress.Close();//закрытии файла и освобождение системных ресурсов
+            File.Move("D:\\KPYP_LECTION\\KPYP_LECTION\\FileIO\\Primer.txt", "D:\\KPYP_LECTION\\KPYP_LECTION\\FileIO\\Primer\\Primer.txt");//переместить файл в указанную папку
+            var adress2 = File.Create("Primer2.txt");
+            adress2.Close();
+            File.Delete("D:\\KPYP_LECTION\\KPYP_LECTION\\FileIO\\Primer\\Primer.txt");//удаление файла
+            if (File.Exists("Primer2.txt"))//если файл2 существует
+            { File.Copy("Primer2.txt", "D:\\KPYP_LECTION\\KPYP_LECTION\\FileIO\\new\\Primer2.txt", true); }//скопировать в указанную папку
         }
     }
 }
