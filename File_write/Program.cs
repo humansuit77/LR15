c342c8cce40e548dcba6bb2ec4536349592b8667
doroshinsky <doroshinsky@mail.ru>
2022-01-12 11:02:53 +0300

    1

--- File_write/Program.cs
+++ File_write/Program.cs
@@ -5,8 +5,9 @@ namespace File_write
     {
         static void Main(string[] args)
         {
-            File.WriteAllText("D:\\KPYP_LECTION\\KPYP_LECTION\\File_write\\Primer.txt", "текст");
-            File.AppendAllText("D:\\KPYP_LECTION\\KPYP_LECTION\\File_write\\Primer.txt", "текст метода AppendAllText ()");
+            string str = "My text";
+            File.WriteAllText("D:\\KPYP_LECTION\\KPYP_LECTION\\File_write\\Primer.txt", str);
+            File.AppendAllText("D:\\KPYP_LECTION\\KPYP_LECTION\\File_write\\Primer.txt", " текст метода AppendAllText ()");
         }
     }
 }
